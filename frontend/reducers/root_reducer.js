8cb75b8c687c8f803487f0596b91d6b8150c266c
App Academy Student <student@appacademy.io>
2017-05-13 10:47:09 -0700

    Everything rendering and working. No errors. Components are next.

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import PostsReducer from './reducers/posts_reducer';
+import PostsReducer from './posts_reducer';
 
 const rootReducer = combineReducers({
   posts: PostsReducer
