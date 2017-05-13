8df7fc3669381d48390c49a6b1e160b52786fe5d
App Academy Student <student@appacademy.io>
2017-05-13 10:42:23 -0700

    posts reducer done

--- frontend/reducers/posts_reducer.js
+++ frontend/reducers/posts_reducer.js
@@ -1,4 +1,5 @@
 import { RECEIVE_POST, RECEIVE_POSTS } from '../actions/post_actions';
+import merge from 'lodash/merge';
 
 const defaultState = {
   title: "",
@@ -9,11 +10,12 @@ const PostsReducer = (state = defaultState, action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_POST:
-
+      let newPost = {[action.post.id]: action.post};
+      return merge({}, state, newPost);
     case RECEIVE_POSTS:
-    
+      return merge({}, action.posts);
     default:
-      return state
+      return state;
   }
 };
 
